032126ce4dd5e0af2b14299f05d1b7cfc7d89999
Smithy1232 <holypigs145@gmail.com>
2026-05-30 21:03:41 -0700

    Read data updated to read the inputted path, so it can intake the daily csv from the dataset, clean data updated with correct types.

--- reader.py
+++ reader.py
@@ -1,17 +1,26 @@
 import pandas as pd
 
-data = pd.read_csv("/Users/aadi/Documents/WA_Fn-UseC_-Telco-Customer-Churn.csv")
-print("CSV loaded")
 
-def clean_data(data):
+def read_data(path: str) -> pd.DataFrame:
+    data = pd.read_csv(path)
+    print("CSV loaded")
+    return data
+
+def clean_data(data:pd.DataFrame) -> pd.DataFrame:
     print("Data Cleaning")
 
     data = data.drop_duplicates()
+    data = data.replace(["", " "], None)
     data = data.dropna()
-    data = data.drop("")
-    #Clamps value b/w 0,1
-    data["Senior Citizen"] = data["Senior Citizen"].clip(lower = 0, upper = 1)
-    #Tenure can't be <0
-    data["Tenure"] = data["Tenure"].clip(lower=0)
 
+    #Clamps value b/w 0,1
+    data["SeniorCitizen"] = data["SeniorCitizen"].clip(lower = 0, upper = 1)
+    #Tenure can't be <0
+    data["tenure"] = data["tenure"].clip(lower=0)
+
+    return data
+
+
+data = clean_data(data)
+print(data.head())
 
